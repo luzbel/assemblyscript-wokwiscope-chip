3fbbd36f144efd27bea8983aae4c3cb417c2ddf3
luzbel <luzbel@users.noreply.github.com>
2024-10-25 12:40:54 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,13 @@ import { EDGE_BOTH, INPUT, OUTPUT, println } from './wokwi-api';
 import { IPinChangeListener, WokwiPin } from './wokwi-pin';
 export { __wokwi_api_version_1 } from './wokwi-api';
 
+import { fetch, Response } from "as-fetch";
+
+export async function fetchText(): Promise<string> {
+  const response: Response = await fetch("http://httpbin.org/get");
+  return await response.text();
+}
+
 class InverterChip implements IPinChangeListener {
   pinIn: WokwiPin;
   pinOut: WokwiPin;
@@ -12,7 +19,7 @@ class InverterChip implements IPinChangeListener {
     this.pinOut.write(!this.pinIn.read());
     this.pinIn.watch(this, EDGE_BOTH);
   }
-
+  
   pinChanged(pin: WokwiPin, newValue: boolean): void {
     println(`Pin ${pin.name} changed to ${newValue}`);
     this.pinOut.write(!newValue);
@@ -21,5 +28,9 @@ class InverterChip implements IPinChangeListener {
 
 export function chipInit(): void {
   println('Hello from inverter chip!');
+  fetchText().then((text: string) => {
+  println(text);
+}).catch((error: Error) => {
+  println("Error fetching text:", error);
   new InverterChip();
 }
